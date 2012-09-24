5d81e03ed0136278d14b91221c5cdc1ebf28eaa2
edmistj <jonedmiston@ccvonline.com>
2012-09-24 16:06:48 -0700

    Added support for Localytics

--- android/src/com/ccvonline/ccvmobileapp/MainActivity.java
+++ android/src/com/ccvonline/ccvmobileapp/MainActivity.java
@@ -4,18 +4,44 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import org.apache.cordova.*;
+import com.localytics.android.*;
 
 public class MainActivity extends DroidGap {
 
-    @Override
+	private LocalyticsSession localyticsSession;
+	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
+        
+        // setup localytics object
+        this.localyticsSession = new LocalyticsSession(
+        		this.getApplicationContext(),  
+                "d7089dd03cf23289031ecd7-e6933f0a-011b-11e2-54de-00ef75f32667");       
+
+        this.localyticsSession.open();               
+        this.localyticsSession.upload(); 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        this.localyticsSession.open();
+    }
+    
+    @Override
+    public void onPause()
+    {
+        this.localyticsSession.close();
+        this.localyticsSession.upload();
+        super.onPause();
     }
 }
