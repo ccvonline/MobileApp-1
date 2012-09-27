b5c131efaa508ebc7c44248c39ed974ffce03408
edmistj <jonedmiston@ccvonline.com>
2012-09-27 00:08:39 -0700

    Nightly changes

--- android/src/com/ccvonline/ccvmobileapp/MainActivity.java
+++ android/src/com/ccvonline/ccvmobileapp/MainActivity.java
@@ -13,7 +13,8 @@ public class MainActivity extends DroidGap {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");
+        super.setIntegerProperty("splashscreen", R.drawable.splash);
+        super.loadUrl("file:///android_asset/www/index.html", 5000);
         
         // setup localytics object
         this.localyticsSession = new LocalyticsSession(
